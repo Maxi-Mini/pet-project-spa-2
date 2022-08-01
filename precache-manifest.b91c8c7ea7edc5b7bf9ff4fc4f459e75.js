8ea53771c398ad75ce44863d85891f41d7d9a4cc
Maxim Tarahov <tarahovmaksim@yandex.ru>
2022-08-01 17:35:58 +0400

    Updates

---
+++ precache-manifest.b91c8c7ea7edc5b7bf9ff4fc4f459e75.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f69ebe576c279cc9b99",
+    "url": "/pet-project-spa-2/static/css/main.d54d2607.chunk.css"
+  },
+  {
+    "revision": "7f69ebe576c279cc9b99",
+    "url": "/pet-project-spa-2/static/js/main.7f69ebe5.chunk.js"
+  },
+  {
+    "revision": "cc79fa3910fc97852656",
+    "url": "/pet-project-spa-2/static/js/1.cc79fa39.chunk.js"
+  },
+  {
+    "revision": "252d17dc35da6d334eb8",
+    "url": "/pet-project-spa-2/static/js/2.252d17dc.chunk.js"
+  },
+  {
+    "revision": "e48e02d87236b8030ed3",
+    "url": "/pet-project-spa-2/static/js/runtime~main.e48e02d8.js"
+  },
+  {
+    "revision": "f5cdadb151232e9e407ac513c59b8706",
+    "url": "/pet-project-spa-2/static/media/circle arrow.f5cdadb1.svg"
+  },
+  {
+    "revision": "efed5ed96270463cdf9a2f6aea05c41a",
+    "url": "/pet-project-spa-2/static/media/arrow.efed5ed9.svg"
+  },
+  {
+    "revision": "6b2b2dbf91f3d014d0c07c27c88aec5d",
+    "url": "/pet-project-spa-2/static/media/header-logo.6b2b2dbf.svg"
+  },
+  {
+    "revision": "7ddb3e29256a236a7fca7ffa958cbd07",
+    "url": "/pet-project-spa-2/static/media/1.7ddb3e29.svg"
+  },
+  {
+    "revision": "524d0ccb39537f3ebdf7fbee5d0d63dd",
+    "url": "/pet-project-spa-2/static/media/2.524d0ccb.svg"
+  },
+  {
+    "revision": "325a635dfcc756b54768731b3c5cf0ed",
+    "url": "/pet-project-spa-2/static/media/phone button.325a635d.svg"
+  },
+  {
+    "revision": "d6a5c402b77ea29b987b65e9ad34b6a8",
+    "url": "/pet-project-spa-2/static/media/people 1.d6a5c402.svg"
+  },
+  {
+    "revision": "b654a677510232a1969117c32155aacc",
+    "url": "/pet-project-spa-2/static/media/people 2.b654a677.svg"
+  },
+  {
+    "revision": "75be99c18b62097a5afb24abe261f05f",
+    "url": "/pet-project-spa-2/static/media/people 3.75be99c1.svg"
+  },
+  {
+    "revision": "89f6d5dcc71e10f582e469f343e69488",
+    "url": "/pet-project-spa-2/static/media/modern logo.89f6d5dc.svg"
+  },
+  {
+    "revision": "6c10c49bb3141cecf416534559bcd091",
+    "url": "/pet-project-spa-2/static/media/vs logo.6c10c49b.svg"
+  },
+  {
+    "revision": "46525f34ed06a2611ad297450ac5bbc2",
+    "url": "/pet-project-spa-2/static/media/brand logo.46525f34.svg"
+  },
+  {
+    "revision": "fb78d8d4b8fa6f9338199905edae5e82",
+    "url": "/pet-project-spa-2/static/media/nature logo.fb78d8d4.svg"
+  },
+  {
+    "revision": "e8a933597a0d109b719d7ac2c974a526",
+    "url": "/pet-project-spa-2/static/media/classic logo.e8a93359.svg"
+  },
+  {
+    "revision": "fab6834eca760ea27716f8a7eb232283",
+    "url": "/pet-project-spa-2/static/media/3.fab6834e.svg"
+  },
+  {
+    "revision": "848829055e2b9b794b2aede19d41e206",
+    "url": "/pet-project-spa-2/static/media/4.84882905.svg"
+  },
+  {
+    "revision": "5063bd876dbf2403aedb092f8911e328",
+    "url": "/pet-project-spa-2/static/media/5.5063bd87.svg"
+  },
+  {
+    "revision": "ce995655789e9594d1cd54a598f60da7",
+    "url": "/pet-project-spa-2/static/media/6.ce995655.svg"
+  },
+  {
+    "revision": "323235856d8d1149ddd9434e1a94b9f9",
+    "url": "/pet-project-spa-2/static/media/7.32323585.svg"
+  },
+  {
+    "revision": "0a069f6d853f868747f3484a7a959f59",
+    "url": "/pet-project-spa-2/static/media/8.0a069f6d.svg"
+  },
+  {
+    "revision": "e0f5439b0f8f1ecd7dc2b0b4deb31f6a",
+    "url": "/pet-project-spa-2/static/media/9.e0f5439b.svg"
+  },
+  {
+    "revision": "c31c245a5f5e7533b85ded7f9320b216",
+    "url": "/pet-project-spa-2/static/media/10.c31c245a.svg"
+  },
+  {
+    "revision": "7aaea4b59ed1d916a3eb2e95bd75a6d5",
+    "url": "/pet-project-spa-2/static/media/11.7aaea4b5.svg"
+  },
+  {
+    "revision": "ca23f9c07cb4baa18158832890a9e90f",
+    "url": "/pet-project-spa-2/static/media/12.ca23f9c0.svg"
+  },
+  {
+    "revision": "6be779d3c965038cff0bd47c4d59b54d",
+    "url": "/pet-project-spa-2/static/media/black arrow.6be779d3.svg"
+  },
+  {
+    "revision": "92cb1eb51eaf96c61278588803352d44",
+    "url": "/pet-project-spa-2/static/media/facebook logo.92cb1eb5.svg"
+  },
+  {
+    "revision": "fbb8649e67ba36a12b2f67d1692cb410",
+    "url": "/pet-project-spa-2/static/media/twitter logo.fbb8649e.svg"
+  },
+  {
+    "revision": "ccc355db72cd185a9a604dd00f92e2a3",
+    "url": "/pet-project-spa-2/static/media/in logo.ccc355db.svg"
+  },
+  {
+    "revision": "744796d2b8524405e4195ca5114cae01",
+    "url": "/pet-project-spa-2/static/media/instagram logo.744796d2.svg"
+  },
+  {
+    "revision": "6ac1fe7aec8c8d5a2ed397d039c4812e",
+    "url": "/pet-project-spa-2/static/media/DMSerifDisplay-Regular.6ac1fe7a.woff2"
+  },
+  {
+    "revision": "2b067ab2e5f5a3ec80cfd781913683c8",
+    "url": "/pet-project-spa-2/static/media/Jost-Regular.2b067ab2.woff2"
+  },
+  {
+    "revision": "03fc8baa941e084d9da8b29af1db810f",
+    "url": "/pet-project-spa-2/index.html"
+  }
+];
